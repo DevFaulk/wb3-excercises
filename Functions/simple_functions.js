fae6e71ba89126fe8f68596d978dd98a3a65c4fe
DevFaulk <faulkdev@gmail.com>
2023-10-23 13:49:45 -0400

    add function example, add function example folder

---
+++ Functions/simple_functions.js
@@ -0,0 +1,18 @@
+function favoriteThings() {
+  console.log("Favorite Song: Sistanem");
+  console.log("Favorite Movie: N/A");
+  console.log("Favorite Musician: J.I.D.");
+}
+
+function whyImHere() {
+  console.log(
+    "I have big dreams of becoming a full stack developer, and learning from the best. From there I'd like to move into other coding languages that work with problem-solving apps. Finally I really want to learn game-design, and game developing."
+  );
+}
+
+function favoritePlace() {
+  console.log(
+    "My favorite place to visit is my bed. Sleep is the secret cheatcode to productivity, and getting a lot done in a day."
+  );
+}
+whyImHere();
